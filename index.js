dfac3d1d9ca7533ed6e9d5d2e4d508b5b391a312
ayala <talmida.Ayaly.Loifer@byb.org.il>
2025-12-18 01:52:33 +0200

    update package.json file

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 const RENDER_API_KEY = process.env.RENDER_API_KEY; // API Key שנשמור בקובץ .env
 const RENDER_API_URL = "https://api.render.com/v1/services"; // URL של ה-API של Render
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Render Services API');
+});
 // Endpoint שמחזיר את רשימת האפליקציות המותקנות ב-Render
 app.get('/services', async (req, res) => {
     try {
